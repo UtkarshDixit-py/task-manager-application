9f10013d95f893e2d989cc8c34e3f84eb5c6055d
UtkarshDixit-py <utkarshdixit303@gmail.com>
2023-06-24 17:06:24 +0530

    /tasks:id route set

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -3,11 +3,18 @@ const tasksRoutes = express.Router();
 const app = express();
 const port = 3002;
 const cors = require('cors');
+const tasksList = require('../tasksList.json');
 
-const taskList = ["todo1","todo2","todo3","todo4"];
 
 tasksRoutes.get('/',(req,res)=>{
-    res.status(200).send(taskList);
+    res.status(200).send(tasksList);
+})
+
+tasksRoutes.get('/:id',(req,res)=>{
+    let taskId = req.params.id;
+    let matchedTask = tasksList.tasks.find((el)=>el.id == taskId);
+
+    res.status(200).send(matchedTask);
 })
 
 
